6769579b8945dd8bc47c77aa5c9710dbba894783
jrjramis984@gmail.com <jaime.ramis1995@gmail.com>
2025-01-10 18:13:56 +0100

    Main files of the project

---
+++ src/App.jsx
@@ -0,0 +1,5 @@
+import Prueba from "./components/prueba";
+
+export default function App() {
+  return <><Prueba></Prueba></>;
+}
---
+++ src/components/prueba.jsx
@@ -0,0 +1,7 @@
+export default function Prueba() {
+  return (
+    <h1 className="bg-sky-500 text-center text-2xl font-bold">
+      Hola soy una PRUEBA EXITOSA!
+    </h1>
+  );
+}
